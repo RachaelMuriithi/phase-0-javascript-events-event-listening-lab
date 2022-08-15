f1bc7c51f57afa4e9a2d45ffc59a6099c33b1502
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-08-15 14:56:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+
+// function addingEventListener() {
+
+//     document.querySelector('#button').addEventListener('click', () => alert("I was clicked"));
+// }
+
 function addingEventListener() {
+    function alertMe () {
+        alert ("I was clicked");
+    }
+    const input = document.getElementById('button');
+    input.addEventListener ('click', alertMe);
 }
